fbcb6893b8efaa39ce8ee6d4ae7d0a93535b7dc4
lhphu <23130251@st.hcmuaf.edu.vn>
2025-12-03 19:40:45 +0700

    push trang tablet

--- TechStore/store/views.py
+++ TechStore/store/views.py
@@ -9,3 +9,8 @@ def home(request):
         "store/images/img3.png",
     ]
     return render(request, 'store/home.html', {"images": images})
+from django.shortcuts import render
+
+def tablet_page(request):
+    return render(request, 'includes/tablet.html')
+
