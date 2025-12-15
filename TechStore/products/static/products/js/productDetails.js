ca9501c702ef219b1b6c52e7614ed230cbb22424
Le Van Truong <23130357@st.hcmuaf.edu.vn>
2025-12-15 17:23:57 +0700

    them comment

--- TechStore/products/static/products/js/productDetails.js
+++ TechStore/products/static/products/js/productDetails.js
@@ -1,3 +1,26 @@
 function changeImage(img) {
     document.getElementById('mainImage').src = img.src;
 }
+const writeBtn = document.getElementById("writeReviewBtn");
+        const form = document.getElementById("reviewForm");
+        const submitBtn = document.getElementById("submitReview");
+        const commentList = document.querySelector(".comment-list");
+
+        // Bấm Write a Review
+        writeBtn.addEventListener("click", () => {
+        form.style.display = "block";
+        writeBtn.style.display = "none";
+    });
+
+        //  Bấm Hoàn thành
+        submitBtn.addEventListener("click", () => {
+        const name = document.getElementById("reviewName").value;
+        const stars = document.getElementById("reviewStars").value;
+        const text = document.getElementById("reviewText").value;
+
+        if (!name || !text) {
+        alert("Vui lòng nhập đầy đủ thông tin.");
+        return;
+    }
+
+    });
