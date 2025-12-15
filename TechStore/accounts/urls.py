5c781db11b71ffcef48a8d3b84fd014b39ca5a0a
ghuy285 <23130134@st.hcmuaf.edu.vn>
2025-12-15 15:08:55 +0700

    logic add product to cart

--- TechStore/accounts/urls.py
+++ TechStore/accounts/urls.py
@@ -1,9 +1,13 @@
 # accounts/urls.py
 from django.urls import path
-from .views import signup_view, login_view
-
+from .views import signup_view, login_view, cart_view, checkout_view, add_to_cart ,buy_now, cart_remove
 urlpatterns = [
     path("signup/", signup_view, name="signup"),
     path("login/", login_view, name="login"),
+    path("cart/", cart_view, name="cart"),
+    path("checkout/", checkout_view, name="checkout"),
+    path("add/<str:product_id>/", add_to_cart, name="add_to_cart"),
+    path("buy-now/<str:product_id>/", buy_now, name="buy_now"),
+    path("remove/<str:item_id>/", cart_remove, name="cart_remove"),
 
 ]
