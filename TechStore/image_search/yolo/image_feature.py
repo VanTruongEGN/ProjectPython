015de2ec77547fbd174acee35ca60bb873772104
Le Van Truong <23130357@st.hcmuaf.edu.vn>
2026-01-22 21:48:42 +0700

    done

--- TechStore/image_search/yolo/image_feature.py
+++ TechStore/image_search/yolo/image_feature.py
@@ -1,19 +1,41 @@
+import torch
 import numpy as np
-from tensorflow.keras.applications import MobileNetV2
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from PIL import Image
+import open_clip
 
-model = MobileNetV2(
-    weights="imagenet",
-    include_top=False,
-    pooling="avg"
+from image_search.yolo.model import model
+
+
+# Device
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+clip_model, _, preprocess = open_clip.create_model_and_transforms(
+    'ViT-B-32',
+    pretrained='openai'
 )
+clip_model = clip_model.to(device)
+clip_model.eval()
 
-def extract_feature(img_path):
-    img = image.load_img(img_path, target_size=(224, 224))
-    x = image.img_to_array(img)
-    x = np.expand_dims(x, axis=0)
-    x = preprocess_input(x)
 
-    feature = model.predict(x, verbose=0)
-    return feature[0]
+def extract_feature(image_path):
+    img = Image.open(image_path).convert("RGB")
+
+    results = model(image_path)
+
+    if results and len(results[0].boxes) > 0:
+        # lấy box có confidence cao nhất
+        boxes = results[0].boxes
+        best_box = max(boxes, key=lambda b: float(b.conf[0]))
+
+        x1, y1, x2, y2 = map(int, best_box.xyxy[0].cpu().numpy())
+        img = img.crop((x1, y1, x2, y2))
+
+    img_tensor = preprocess(img).unsqueeze(0).to(device)
+
+    with torch.no_grad():
+        feature = clip_model.encode_image(img_tensor)
+
+    feature = feature.cpu().numpy().flatten()
+    feature = feature / np.linalg.norm(feature)
+
+    return feature
